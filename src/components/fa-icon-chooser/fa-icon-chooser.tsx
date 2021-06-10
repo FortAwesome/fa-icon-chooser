ce78e80b4f235a305c7d3bbf68433e4f926e5697
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-06-10 13:36:37 -0700

    change API for component event handler props

--- src/components/fa-icon-chooser/fa-icon-chooser.tsx
+++ src/components/fa-icon-chooser/fa-icon-chooser.tsx
@@ -7,10 +7,9 @@ export interface IconChooserResult extends IconLookup {
   style?: string;
 }
 
-export interface IconChooserEventHandler {
-  handleQuery: (document: string) => Promise<any>;
-  handleResult: (result: IconChooserResult) => void;
-};
+export type QueryHandler = (document: string) => Promise<any>;
+
+export type ResultHandler = (result: IconChooserResult) => void;
 
 @Component({
   tag: 'fa-icon-chooser',
@@ -35,7 +34,9 @@ export class FaIconChooser {
    */
   @Prop() enablePro: boolean;
 
-  @Prop() eventHandler: IconChooserEventHandler;
+  @Prop() onQuery: QueryHandler;
+
+  @Prop() onResult: ResultHandler;
 
   @State() query: string;
 
@@ -57,7 +58,7 @@ export class FaIconChooser {
     this.isQuerying = true
     const version = '5.15.3'
     const usingPro = true
-    this.eventHandler.handleQuery(
+    this.onQuery(
       `
       query {
         search(version:"${version}", query: "${query}", first: 10) {
@@ -100,7 +101,7 @@ export class FaIconChooser {
             : (size(this.queryResults) > 0
                 ?  this.queryResults.map(result =>
                     <article class="wrap-icon" key={ `fas-${ result.id }`}>
-                    <button class="icon subtle display-flex flex-column flex-items-center flex-content-center" onClick={() => this.eventHandler.handleResult({ prefix: 'fas', iconName: result.id})}>
+                    <button class="icon subtle display-flex flex-column flex-items-center flex-content-center" onClick={() => this.onResult({ prefix: 'fas', iconName: result.id})}>
                         <i class={ `fas fa-2x fa-${result.id}` }></i>
                       <span class="icon-name size-xs text-truncate margin-top-lg">{`${result.id}`}</span>
                       </button>
