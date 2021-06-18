daaa79ceadbca42f82e57e60b0368a9fd2ee9dd8
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-06-18 11:27:27 -0700

    initial setup of fa-icon-chooser-react

---
+++ packages/fa-icon-chooser-react/src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@fortawesome/fa-icon-chooser';
+
+import { defineCustomElements } from '@fortawesome/fa-icon-chooser/loader';
+
+defineCustomElements();
+export const FaIconChooser = /*@__PURE__*/createReactComponent<JSX.FaIconChooser, HTMLFaIconChooserElement>('fa-icon-chooser');
