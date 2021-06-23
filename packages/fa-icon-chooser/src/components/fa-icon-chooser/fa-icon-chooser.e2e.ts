5efc8e795c2b1abcd30da07cbebc83adc942fc0d
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-06-23 12:20:22 -0700

    Add test comments

--- packages/fa-icon-chooser/src/components/fa-icon-chooser/fa-icon-chooser.e2e.ts
+++ packages/fa-icon-chooser/src/components/fa-icon-chooser/fa-icon-chooser.e2e.ts
@@ -1,5 +1,12 @@
 import { newE2EPage } from '@stencil/core/testing'
 
+// TODO: add test matrix to ensure that icons and styles are being applied,
+// producing the expected rendering for:
+// - Kit with SVG/JS
+// - Kit with Webfont/CSS
+// - CDN Webfont/CSS
+// - CDN SVG/JS
+
 describe('fa-icon-chooser', () => {
   it('renders', async () => {
     const page = await newE2EPage()
