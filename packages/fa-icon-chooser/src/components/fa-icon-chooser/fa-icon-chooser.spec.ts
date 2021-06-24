e6b48b1d7feb69e569eac5eb7a75e4110c61522f
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-06-24 15:44:09 -0700

    improve basic unit test with actual rendering

--- packages/fa-icon-chooser/src/components/fa-icon-chooser/fa-icon-chooser.spec.ts
+++ packages/fa-icon-chooser/src/components/fa-icon-chooser/fa-icon-chooser.spec.ts
@@ -10,11 +10,8 @@ async function mountWith(params) {
   const { attrs, handleQuery, handleResult } = params
 
   const page = await newSpecPage({
-    components: [FaIconChooser],
-    html: '<div id="container"></div>',
+    components: [FaIconChooser]
   })
-
-  const container = page.doc.querySelector('#container')
 
   const el = document.createElement('fa-icon-chooser')
   el.handleQuery = handleQuery || jest.fn()
@@ -24,15 +21,22 @@ async function mountWith(params) {
     el.setAttribute(attr, attrs[attr])
   }
 
-  container.appendChild(el)
+  page.body.appendChild(el)
   await page.waitForChanges()
 
   return page
 }
 
 describe('fa-icon-chooser', () => {
-  it('renders', async () => {
-    const page = await mountWith({ attrs: {version: '5.15.3'}})
-    expect(page.root.innerHTML).toMatch("<h2>Sorry, we couldn't find anything for that...</h2>")
+  const handleQuery = jest.fn().mockReturnValueOnce(Promise.resolve(
+    {"data":{"search":[{"id":"business-time","label":"Business Time","membership":{"free":["solid"],"pro":["solid","regular","light","duotone"]}},{"id":"socks","label":"Socks","membership":{"free":["solid"],"pro":["solid","regular","light","duotone"]}},{"id":"print-slash","label":"Print Slash","membership":{"free":[],"pro":["solid","regular","light","duotone"]}},{"id":"print-search","label":"Print Search","membership":{"free":[],"pro":["solid","regular","light","duotone"]}},{"id":"print","label":"print","membership":{"free":["solid"],"pro":["solid","regular","light","duotone"]}},{"id":"fax","label":"Fax","membership":{"free":["solid"],"pro":["solid","regular","light","duotone"]}},{"id":"user-tie","label":"User Tie","membership":{"free":["solid"],"pro":["solid","regular","light","duotone"]}},{"id":"building","label":"Building","membership":{"free":["solid","regular"],"pro":["solid","regular","light","duotone"]}},{"id":"briefcase","label":"Briefcase","membership":{"free":["solid"],"pro":["solid","regular","light","duotone"]}}]}}
+  ))
+
+  it('renders with initial icons', async () => {
+    const page = await mountWith({ handleQuery, attrs: { version: '5.15.3', 'cdn-url': 'https://example.com/all.js', pro: true } })
+    expect(handleQuery.mock.calls.length).toBe(1)
+    expect(page.root.shadowRoot.innerHTML).toMatch('<div class="fa-icon-chooser">')
+    expect(page.root.shadowRoot.innerHTML).toMatch('fa-business-time')
+    expect(page.root.shadowRoot.innerHTML).toMatch('fa-socks')
   })
 })
