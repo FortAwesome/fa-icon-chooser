5d7ba9f35c7c0b28101c0774ca701d56906a4ad4
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-06-24 10:50:15 -0700

    touch a global in the fakeKit.js

--- packages/fa-icon-chooser/src/dev/fakeKit.js
+++ packages/fa-icon-chooser/src/dev/fakeKit.js
@@ -2,4 +2,5 @@
   const icons = document.querySelectorAll('.icon')
   console.log('DEBUG: fakeKit document.currentScript', document.currentScript)
   console.log('DEBUG: fakeKit .icon selection on document', icons)
+  window.FaIconChooserDevLastTouched = new Date()
 })()
