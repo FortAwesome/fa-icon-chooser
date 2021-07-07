ade0258a770c24582a26550d844647137d960f86
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-07-07 09:07:23 -0700

    refactor faCircleNotch

--- packages/fa-icon-chooser/src/components/fa-icon/fa-icon.tsx
+++ packages/fa-icon-chooser/src/components/fa-icon/fa-icon.tsx
@@ -2,6 +2,7 @@ import { Component, Host, Prop, State, h } from '@stencil/core'
 import { IconPrefix, IconUpload, PREFIX_TO_STYLE, parseSvgText } from '../../utils/utils'
 import { IconDefinition } from '@fortawesome/fontawesome-common-types'
 import { get } from 'lodash'
+import { faCircleNotch } from '../../utils/icons'
 
 @Component({
   tag: 'fa-icon',
@@ -113,22 +114,25 @@ export class FaIcon {
       console.error(`DEBUG: caught error for icon: ${this.name}`, e)
     })
     .finally(() => {
+      debugger
       this.loading = false
     })
   }
 
-  buildSvg(iconDefinition) {
+  buildSvg(iconDefinition: IconDefinition, extraClasses?: string) {
     if(!iconDefinition) return
 
     const [ width, height,,, svgPathData ] = get(iconDefinition, 'icon', [])
 
+    const classes = `svg-inline--fa ${this.class} ${extraClasses}`
+
     if(Array.isArray(svgPathData)) {
-      return (<svg class={`svg-inline--fa ${this.class}`} xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
+      return (<svg class={ classes } xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
         <path fill="currentColor" class="fa-primary" d={ svgPathData[0] }/>
         <path fill="currentColor" class="fa-secondary" d={ svgPathData[1] }/>
       </svg>)
     } else {
-      return (<svg class={`svg-inline--fa ${this.class}`} xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
+      return (<svg class={ classes } xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${width} ${height}`}>
         <path fill="currentColor" d={ svgPathData }/>
       </svg>)
     }
@@ -136,11 +140,7 @@ export class FaIcon {
 
   render() {
     if(this.loading) {
-      return (
-        <Host>
-          <svg class="svg-inline--fa fa-spin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"/></svg>
-        </Host>
-      )
+      return this.buildSvg( faCircleNotch, 'fa-spin' )
     }
 
     return this.iconDefinition
