9ab3134984c5977cfb3aa633ca6f5dcc1d0bb52d
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-07-09 11:35:03 -0700

    remove obsolete resolveVersion function

--- packages/fa-icon-chooser/src/utils/utils.spec.ts
+++ packages/fa-icon-chooser/src/utils/utils.spec.ts
@@ -1,25 +1,9 @@
 import {
-  resolveVersion,
   parseSvgText,
   Icon,
   IconChooserResult,
   Element
 } from './utils'
-
-describe('resolveVersion', () => {
-  it('resolves 5.x', () => {
-    expect(resolveVersion('5.x')).toEqual('5.15.3')
-  })
-  it('resolves 6.x', () => {
-    expect(resolveVersion('6.x')).toEqual('6.0.0-beta1')
-  })
-  it('resolves latest', () => {
-    expect(resolveVersion('latest')).toEqual('5.15.3')
-  })
-  it('resolves 5.15.1', () => {
-    expect(resolveVersion('5.15.1')).toEqual('5.15.1')
-  })
-})
 
 describe('parseSvgText', () => {
   test('duotone double path no classes', () => {
--- packages/fa-icon-chooser/src/utils/utils.ts
+++ packages/fa-icon-chooser/src/utils/utils.ts
@@ -69,20 +69,6 @@ export interface Icon extends IconLookup, Customizable {
 
 export type IconChooserResult = Icon | Element;
 
-// TODO: replace this placeholder logic with, probably, real API calls
-// that handle resolving the version.
-export function resolveVersion(version: string): string {
-  switch(version) {
-    case '5.x':
-    case 'latest':
-      return '5.15.3'
-    case '6.x':
-      return '6.0.0-beta1'
-    default:
-      return version
-  }
-}
-
 // TODO: do we want to include this code in this project?
 const viewBoxRe = /viewBox="0 0 ([0-9]+) ([0-9]+)"/
 const singlePathRe = /path d="([^"]+)"/
