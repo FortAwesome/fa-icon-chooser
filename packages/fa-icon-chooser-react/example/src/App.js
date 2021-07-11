8c85ad5bbf186aa66e645466b18050427283aba9
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-07-11 15:16:56 -0700

    use slots to demonstrate how slots can be used to internationalize UI messages

--- packages/fa-icon-chooser-react/example/src/App.js
+++ packages/fa-icon-chooser-react/example/src/App.js
@@ -2,6 +2,28 @@ import './App.css';
 import { FaIconChooser } from '@fortawesome/fa-icon-chooser-react'
 import { handleQuery, getLocalConfig, getUrlText } from './devRuntime'
 import React, { useState } from 'react'
+import Jed from '@tannin/compat';
+
+/*
+In this example, we'll use slots to override a some of the default English
+language UI messages in the start view with strings that could be prepared
+for translation into other languages using a library like Tannin.
+*/
+const i18n = new Jed( {
+    locale_data: {
+        my_app_text_domain: {
+            '': {
+                domain: 'my_app_text_domain',
+                lang: 'en',
+                plural_forms: 'nplurals=2; plural=(n != 1);',
+            },
+        },
+    },
+    domain: 'my_app_text_domain',
+} );
+
+const startViewHeading = i18n.translate("Font Awesome is the web's most popular icon set, with tons of icons in a variety of styles.").fetch();
+const startViewDetail = i18n.translate("Not sure where to start? Here are some favorites, or try a search for spinners, animals, food, or whatever you're looking for.").fetch();
 
 function App() {
   const [showingChooser, showChooser] = useState(false)
@@ -34,6 +56,12 @@ function App() {
             handleQuery={ handleQuery }
             getUrlText={ getUrlText }
             onFinish={ handleResult }>
+              <span slot="start-view-heading">
+                { startViewHeading }
+              </span>
+              <span slot="start-view-detail">
+                { startViewDetail }
+              </span>
           </FaIconChooser>
         }
       </header>
