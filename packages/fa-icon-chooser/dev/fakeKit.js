1a66a88aaf2b6f07c1e42f60c02a43e6e3cbaf01
Mike Wilkerson <11575183+mlwilkerson@users.noreply.github.com>
2021-07-15 16:33:32 -0500

    remove fakeKit.js

--- packages/fa-icon-chooser/dev/fakeKit.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  const icons = document.querySelectorAll('.icon')
-  console.log('DEBUG: fakeKit document.currentScript', document.currentScript)
-  console.log('DEBUG: fakeKit .icon selection on document', icons)
-  window.FaIconChooserDevLastTouched = new Date()
-})()
